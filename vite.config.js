ce6c056ae518990acffd555a1a75329ab7075d13
qepsen <ludwigerikssoon@hotmail.com>
2026-04-10 09:35:25 +0200

    remove comments in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
  
 export default defineConfig({
-  // IMPORTANT: Replace 'salary-calc' with your actual GitHub repo name
   base: '/https://github.com/Medieinstitutet/sys25d-devops-inl-1-qepsen.git',
   test: {
     environment: 'node',
